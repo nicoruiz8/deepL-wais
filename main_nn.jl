53f55bcfc9c29d621d7175702bac9bca18d9ce1c
Nicolás Ruiz Lafuente <137712281+nicoruiz8@users.noreply.github.com>
2023-06-26 19:37:06 +0200

    Neural network building and training

--- main_nn.jl
+++ main_nn.jl
@@ -4,7 +4,7 @@
 ## Author: Nicolás Ruiz Lafuente
 ## Special thanks to Jan-Christophe Swierczek-Jereczek for his invaluable help and expertise in the matter.
 
-## This script just builds and trains a custom neural network. For testing results check the README.
+## This script just builds and trains a custom neural network. For testing results check the README file.
 
 using Flux, CairoMakie, ProgressBars, Random, BSON
 
